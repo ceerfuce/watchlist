755bb4d0d1b2df722c43edcc1a86f5ab5edde690
ceerfuce <ceerfuce@outlook.com>
2019-03-09 20:47:26 +0800

    no block use in html version
    bak for later change

---
+++ app.py
@@ -0,0 +1,130 @@
+import os
+import sys
+
+import click
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+# SQLite URI compatible
+WIN = sys.platform.startswith('win')
+if WIN:  # 如果是 Windows 系统，使用三个斜线
+    prefix = 'sqlite:///'
+else:  # 否则使用四个斜线
+    prefix = 'sqlite:////'
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+# 在扩展类实例化前加载配置
+db = SQLAlchemy(app)
+
+
+@app.cli.command()  # 注册为命令
+@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
+def initdb(drop):
+    """Initialize the database."""
+    if drop:  # 判断是否输入了选项
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')  # 输出提示信息
+
+
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.create_all()
+    # 全局的两个变量移动到这个函数内
+    name = 'Grey Li'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Done.')
+
+
+# 表名将会是 user （自动生成，小写处理）
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+
+
+# 表名将会是 movie
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
+
+
+'''    
+name = 'Grey Li' 
+
+movies = [ 
+    {'title': 'My Neighbor Totoro', 'year': '1988'}, 
+    {'title': 'Dead Poets Society', 'year': '1989'}, 
+    {'title': 'A Perfect World', 'year': '1993'}, 
+    {'title': 'Leon', 'year': '1994'}, 
+    {'title': 'Mahjong', 'year': '1996'}, 
+    {'title': 'Swallowtail Butterfly', 'year': '1996'}, 
+    {'title': 'King of Comedy', 'year': '1999'}, 
+    {'title': 'Devils on the Doorstep', 'year': '1999'}, 
+    {'title': 'WALL-E', 'year': '2008'}, 
+    {'title': 'The Pork of Music', 'year': '2012'}, 
+] 
+'''
+
+'''
+对于多个模板内都需要使用的变量，
+我们可以使用app.context_processor装饰器注册一个
+模板上下文处理函数
+这个函数返回的变量（以字典键值对的形式）
+将会统一注入到每一个模板的上下文环境中，
+因此可以直接在模板中使用。
+'''
+@app.context_processor 
+def inject_user():  # 函数名可以随意修改 
+    user = User.query.first() 
+    return dict(user=user)  # 需要返回字典，等同于return {'user': user}
+
+@app.route('/')
+def index():
+    # user = User.query.first()  # 读取用户记录
+    movies = Movie.query.all()  # 读取所有电影记录
+    # return render_template('index.html', user=user, movies=movies)
+    # 使用模板上下文处理函数后，删除user变量定义，
+    # 并删除在render_template函数里传入的关键字参数
+    return render_template('index.html', movies=movies)
+    
+    
+'''
+使用app.errorhandler装饰器注册一个错误处理函数，
+它的作用和视图函数类似，当 404 错误发生时，这个函数会被触发，
+返回值会作为响应主体返回给客户端
+'''
+@app.errorhandler(404)  # 传入要处理的错误代码
+def page_not_found(e):  # 接受异常对象作为参数
+    return render_template('404.html'), 404  # 返回模板和状态码
+      
+@app.route('/hello')
+def hello_world():
+    return 'Hello, World!'
+
+
+@app.route('/user/<name>')
+def hello_user(name):
+    return 'Hello, user %s .' % name
---
+++ fund.bat
@@ -0,0 +1,2 @@
+py -3 -m flask run
+pause
